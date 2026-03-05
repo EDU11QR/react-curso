18858c26927af7277464a9a3ba1051ecff1b49f5
DevEdu <quisperojasedu@gmail.com>
2026-03-04 23:56:13 -0500

    modificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export function App(){
+    return(
+        <article style={{ display: "flex", alignItems: "center" ,color: "#fff"}}>  
+            <header>
+                <img alt="El Avatar de midude" src="https://unavatar.io/midudev"/>
+                <div>
+                    <strong>Miguel Angel</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
 }
-
-export default App
