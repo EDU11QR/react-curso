b92ca2e3853e58323825964963e588fa1d79bfdc
DevEdu <quisperojasedu@gmail.com>
2026-03-08 18:03:35 -0500

    feat: agregando funcionalidad al juego y cuando gana se detiene

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -19,20 +19,57 @@ const Square = ({children, isSlected, updateBoard, index }) =>{
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+
+]
+
 function App() {
   const [board, setBoard] = useState(
     Array(9).fill(null)
   )
 
   const [turn, setTurn] = useState(TURNS.X)
+   // es que no hay ganador, false es que hay un empate
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for(const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if(
+        boardToCheck[a] && // 0 -> x u o
+        boardToCheck[a] == boardToCheck[b] &&
+        boardToCheck[a] == boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    //si no hay ganador
+    return null
+  }
 
   const updateBoard = (index) =>{
+    //no actualizar esta posicion si ya tiene algo
+    if (board[index] || winner) return
+    //actualizar el tablero
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-
+    //cambiar de turno
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    }
   }
 
   return (
