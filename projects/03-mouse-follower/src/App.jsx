4f9c280caadaa10ed432e041b6530d8eb3753e02
DevEdu <quisperojasedu@gmail.com>
2026-03-09 23:37:06 -0500

    feat: guardar cambios, practica exitosa

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react"
 
-function App() {
+const FollowMouse = () => {
+  
   const [enabled, setEnabled] = useState(false)
   const[position, setPosition] = useState({ x: 0, y: 0})
-
+  
+  // pointer move
   useEffect(() => {
     console.log(`effect`,{enabled})
 
@@ -17,30 +19,47 @@ function App() {
     }
 
     return() => {
+      console.log(`cleanup`)
       window.removeEventListener(`pointermove`, handleMove)
     }  
   }, [enabled])
 
+  //change body className
+  useEffect(() => {
+    document.body.classList.toggle(`no-cursor`, enabled)
+    return () =>{
+      document.body.classList.remove(`no-cursor`)
+    }
+  },[enabled])
+  
+  return(
+    <>
+      <div style={{
+        position: `absolute`,
+        backgroundColor: `#c81dd8`,
+        border: `1px solid #fff`,
+        borderRadius: `50%`,
+        opacity: 0.8,
+        pointerEvents: `none`,
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? `Desactivar` : `Activar`} seguir puntero
+      </button>
+    </>
+  )
+}
+
+function App() {
 
   return (
     <main>
-    <div style={{
-      position: `absolute`,
-      backgroundColor: `#c81dd8`,
-      border: `1px solid #fff`,
-      borderRadius: `50%`,
-      opacity: 0.8,
-      pointerEvents: `none`,
-      left: -25,
-      top: -25,
-      width: 50,
-      height: 50,
-      transform: `translate(${position.x}px, ${position.y}px)`
-    }}
-    />
-    <button onClick={() => setEnabled(!enabled)}>
-      {enabled ? `Desactivar` : `Activar`} seguir puntero
-      </button>
+    <FollowMouse />
     </main>
     
   )
